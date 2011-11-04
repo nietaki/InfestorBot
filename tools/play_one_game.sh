a1a520ca6910bd1980c31bc71c52623de29ca336
Jacek Królikowski <nietaki@gmail.com>
2011-11-04 18:34:54 +0100

    sources (and just the sources) moved and reorganized to work well with the CMake Eclipse and Git Out-Of-Source configuration. Less convenient, more versatile

--- tools/play_one_game.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-./playgame.py --player_seed 42 --end_wait=0.25 --verbose --log_dir game_logs --turns 100 --map_file maps/maze/maze_02p_01.map "$@"  "python sample_bots/python/LeftyBot.py" "../bin/InfestorBot.run"
