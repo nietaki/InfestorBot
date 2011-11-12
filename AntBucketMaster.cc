f8266bcd367b8a71c6e8f4be10dbe797d2f6bc3d
Jacek Królikowski <nietaki@gmail.com>
2011-11-12 18:30:52 +0100

    getClosestAntsFromSingleBucket written, not verified

--- AntBucketMaster.cc
+++ AntBucketMaster.cc
@@ -17,8 +17,31 @@ AntBucketMaster::~AntBucketMaster() {
 
 }
 
+//TODO check this test
 AntListPtr AntBucketMaster::getClosestAntsFromSingleBucket(Location inLoc, int max_count) {
+  //this list will have the closest Ants at the end
   AntListPtr list = AntListPtr(new AntList());
+  AntBucket& bucket = getBucket(inLoc);
+  AntBucket::iterator it;
+
+  for(it = bucket.begin(); it != bucket.end(); it++){
+    AntPtr currentAnt = *it;
+    AntList::iterator lit;
+    lit = list->begin();
+    while(lit != list->end() && currentAnt->getLocation().taxiDistance(inLoc, cols, rows)
+                                                  <
+                            (*lit)->getLocation().taxiDistance(inLoc, cols, rows)){
+      lit++;
+    }
+
+
+    list->insert(lit, currentAnt);
+
+    while(list->size() > max_count){
+      list->pop_front();
+    }
+  }
+
 
 
   return list;
